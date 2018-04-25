7fa9ebfa61abc431bd955997c3283495b377dadc
catchMouse <shenzhenxing1@huawei.com>
2018-04-25 13:54:00 +0800

    标记已读的动画

---
+++ TickActivity.java
@@ -0,0 +1,28 @@
+package com.example.pateo.videotest.view;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+
+import com.example.pateo.videotest.R;
+
+public class TickActivity extends Activity {
+
+    TickView tickView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tick);
+
+        tickView = (TickView)findViewById(R.id.tickView);
+    }
+
+    public void read_click(View v) {
+        tickView.setEnabled(false);
+        tickView.setSelected(true);
+    }
+
+}
